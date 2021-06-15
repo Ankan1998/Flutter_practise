5b2bb293bc30c5df0b63626c6bc8fb6fe632df08
Ankan Sharma <ankanroni3@gmail.com>
2021-06-15 16:24:08 +0530

    Reusing the hornbill widget

--- birderapp/lib/widgets/bird_list_with_rows_cols.dart
+++ birderapp/lib/widgets/bird_list_with_rows_cols.dart
@@ -5,6 +5,41 @@ class BirdListWithRowsCols extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Birdlist Widget');
+    return  Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Image(
+                    width: 100.00,
+                    image: AssetImage('images/hornbill.jpg')),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 10.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Hornbill',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold,
+                        )
+                      ),
+                      Text(
+                        'Bucerotidae',
+                        style: TextStyle(
+                          color:Colors.grey[700],
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.italic,
+                        )
+                      )
+                    ],
+                  ),
+                ),
+              
+              ],
+            );
   }
 }
