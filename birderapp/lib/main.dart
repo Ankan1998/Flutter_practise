a51fd707f9cf0bf281c26c16cdf708db66c926b3
Ankan Sharma <ankanroni3@gmail.com>
2021-06-15 16:19:45 +0530

    Making Custom Widget for reusability

--- birderapp/lib/main.dart
+++ birderapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:birderapp/widgets/bird_list_with_rows_cols.dart';
 import 'package:flutter/material.dart';
 // https://codeshare.io/LwnDvb
 
@@ -11,46 +12,52 @@ void main() {
             centerTitle: true,
             backgroundColor: Colors.teal[700],
           ),
-          body: Container(
+          body: Column(
             // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            margin: EdgeInsets.all(10.0),
-
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Image(
-                    width: 100.00,
-                    image: AssetImage('images/hornbill.jpg')),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10.0,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Hornbill',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontFamily: 'Raleway',
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                      Text(
-                        'Bucerotidae',
-                        style: TextStyle(
-                          color:Colors.grey[700],
-                          fontSize: 20.0,
-                          fontStyle: FontStyle.italic,
-                        )
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            children: <Widget>[
+              BirdListWithRowsCols(),
+              BirdListWithRowsCols()
+              
+            ]
           )),
     ),
   );
 }
+
+
+// Row(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               // crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: <Widget>[
+//                 Image(
+//                     width: 100.00,
+//                     image: AssetImage('images/hornbill.jpg')),
+//                 Padding(
+//                   padding: const EdgeInsets.only(
+//                     left: 10.0,
+//                   ),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: <Widget>[
+//                       Text(
+//                         'Hornbill',
+//                         style: TextStyle(
+//                           fontSize: 30.0,
+//                           fontFamily: 'Raleway',
+//                           fontWeight: FontWeight.bold,
+//                         )
+//                       ),
+//                       Text(
+//                         'Bucerotidae',
+//                         style: TextStyle(
+//                           color:Colors.grey[700],
+//                           fontSize: 20.0,
+//                           fontStyle: FontStyle.italic,
+//                         )
+//                       )
+//                     ],
+//                   ),
+//                 ),
+              
+//               ],
+//             ),
---
+++ birderapp/lib/widgets/bird_list_with_rows_cols.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BirdListWithRowsCols extends StatelessWidget {
+  const BirdListWithRowsCols({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Birdlist Widget');
+  }
+}
