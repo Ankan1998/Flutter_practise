d66f4d3fa3a38ef745d00df3e4f3e86650538c32
Ankan Sharma <ankanroni3@gmail.com>
2021-06-15 17:24:08 +0530

    spread operator for list fetching

--- birderapp/lib/widgets/BirdListWithListView.dart
+++ birderapp/lib/widgets/BirdListWithListView.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
 class BirdListWithListView extends StatelessWidget {
-  
+  List<String> listofbirds = ["Hornbill", "parrot", "kingfisher"];
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Text('List1'),
-        Text('List1'),
-        Text('List1'),
+        ...listofbirds.map( //... is a spread operator, creates temporary list and fetches elemeent one by one
+          (bird) => Text(bird),
+        )
       ],
-
     );
   }
 }
