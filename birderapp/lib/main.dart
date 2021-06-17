e6dab01d4f0507038e3dd762d4f3ef41b38a0c57
Ankan Sharma <ankanroni3@gmail.com>
2021-06-17 10:17:03 +0530

    Complete adding of new page and using ChangeNotifier to add new bird to home scrren (make change to UI)

--- birderapp/lib/main.dart
+++ birderapp/lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:birderapp/models/birdlist_changenotifier.dart';
 import 'package:birderapp/widgets/BirderApp.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 // https://codeshare.io/LwnDvb
 // Flutter packages
 //https://pub.dev/
 
 void main() {
   runApp(
-    MaterialApp( //Keeping Mterial App in main page and then navigating accordingly
-    home: BirderApp(),
-    )
+    ChangeNotifierProvider(
+      create: (_) => BirdListChangeNotifier(),
+      child: MaterialApp(
+        home: BirderApp(),
+      ),
+    ),
   );
 }
