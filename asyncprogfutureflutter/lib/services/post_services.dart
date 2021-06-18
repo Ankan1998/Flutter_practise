57628aa29183a8008d7383118f20a0e040144ab5
Ankan Sharma <ankanroni3@gmail.com>
2021-06-18 10:42:19 +0530

    fetching service using json

---
+++ asyncprogfutureflutter/lib/services/post_services.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class PostsServices {
+  void fetchPosts() async {
+    var url = Uri.https('jsonplaceholder.typicode.com',
+        'posts/10'); //first argument only domain, second argument posts
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+    }
+  }
+}
