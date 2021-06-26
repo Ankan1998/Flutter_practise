8664d85adaf3e5ca29a13574f1d74ae6ccdde367
Ankan Sharma <ankanroni3@gmail.com>
2021-06-26 10:17:48 +0530

    bird_services.dart added to get data from firebase realtime database

--- birderapp/lib/services/birds_services.dart
+++ birderapp/lib/services/birds_services.dart
@@ -7,36 +7,7 @@ class BirdsService {
   // addABirdtoFirebaseDB()
   // getAllBirds()
 
-  //   getAllBirds(BirdModel newBird) async {
-  //   try {
-  //     // do not hard code urls is bad practise ! Instead accept them as parameters
-  //     const _domainString =
-  //         "ankansapientbirderapp-default-rtdb.asia-southeast1.firebasedatabase.app";
-  //     const _urlString = "birds.json";
-  //     Uri url = Uri.https(_domainString, _urlString);
-  //     var response = await http.get(
-  //       url,
-  //       body: json.encode({
-  //         "id": newBird.id,
-  //         "name": newBird.name,
-  //         "scientificName": newBird.scientificName,
-  //         "likes": 200,
-  //         "info": newBird.info,
-  //         "url": newBird.imageUrl
-  //       }),
-  //     );
-
-  //     if (response.statusCode == 200) {
-  //       print('Bird Inserted !');
-  //       return "success";
-  //     }
-  //   } catch (e) {
-  //     print(e);
-  //     return e;
-  //   }
-  // }
-
-  addABirdtoFirebaseDB(BirdModel newBird) async {
+  Future<String> addABirdtoFirebaseDB(BirdModel newBird) async {
     try {
       // do not hard code urls is bad practise ! Instead accept them as parameters
       const _domainString =
@@ -51,17 +22,46 @@ class BirdsService {
           "scientificName": newBird.scientificName,
           "likes": 200,
           "info": newBird.info,
-          "url": newBird.imageUrl
+          "imageUrl": newBird.imageUrl
         }),
       );
 
       if (response.statusCode == 200) {
         print('Bird Inserted !');
         return "success";
+      } else {
+        return "";
       }
     } catch (e) {
       print(e);
       return e;
+    }
+  }
+
+  Future<List<BirdModel>> getAllBirdsFromFirebaseDB() async {
+    try {
+      final List<BirdModel> loadedBirds = [];
+      // do not hard code urls is bad practise ! Instead accept them as parameters
+      const _domainString =
+          "ankansapientbirderapp-default-rtdb.asia-southeast1.firebasedatabase.app";
+      const _urlString = "birds.json";
+      Uri url = Uri.https(_domainString, _urlString);
+      var response = await http.get(
+        url,
+      );
+
+      if (response.statusCode == 200) {
+        print('Birds Received !');
+        var extractedBirds = jsonDecode(response.body) as Map<String, dynamic>;
+
+        // {"key":{id:1,.....}}
+        extractedBirds.forEach((birdUniqueKey, eachBird) {
+          loadedBirds.add(BirdModel.fromJSON(eachBird));
+        });
+        return loadedBirds;
+      }
+    } catch (e) {
+      print(e);
     }
   }
 }
